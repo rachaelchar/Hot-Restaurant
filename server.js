557d4f14887065131cde0681c69bb54ab747f679
RachaelChar <charewicz.r@gmail.com>
2019-12-07 11:16:21 -0500

    add basic routes

--- server.js
+++ server.js
@@ -6,3 +6,15 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}.`)
 });
+
+app.get("/home", (req, res) => {
+    res.send("Home Page");
+});
+
+app.get("/reserve", (req, res) => {
+    res.send("Reservations Page");
+});
+
+app.get("/tables", (req, res) => {
+    res.send("Tables Page");
+});
