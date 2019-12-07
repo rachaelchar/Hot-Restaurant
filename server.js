69b0ac087ea624de512bac107b57ddbae6ff5554
RachaelChar <charewicz.r@gmail.com>
2019-12-07 11:23:44 -0500

    handle data parsing

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}.`)
 });
