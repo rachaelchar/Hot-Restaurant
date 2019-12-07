0fb82a34d5c53039be942c6772f8c94fa0687a03
RachaelChar <charewicz.r@gmail.com>
2019-12-07 11:10:07 -0500

    require express & listen on port

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}.`)
+});
